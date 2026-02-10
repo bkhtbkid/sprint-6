8c46c9886fc722b8cca2bfe352eb03e14122a980
baha <kidirov1baha@mail.ru>
2026-02-10 17:23:42 +0500

    sprint-6

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,56 @@
 package handlers
+
+import (
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+func MainHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "index.html")
+}
+
+func UploadHandler(w http.ResponseWriter, r *http.Request) {
+	file, header, err := r.FormFile("file")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	content := string(data)
+	var result string
+
+	if strings.HasPrefix(content, ".") || strings.HasPrefix(content, "-") {
+		result = morse.ToText(content)
+	} else {
+		result = morse.ToMorse(content)
+	}
+
+	filename := time.Now().UTC().String() + filepath.Ext(header.Filename)
+
+	dst, err := os.Create(filename)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer dst.Close()
+
+	if _, err := dst.WriteString(result); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write([]byte(result))
+}
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,34 @@
 package service
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+func DetectFormat(input string) (string, error) {
+	isMorse := true
+
+	index := strings.IndexFunc(input, func(r rune) bool {
+		return r != '-' && r != '.' && r != ' '
+	})
+
+	if index == -1 {
+		isMorse = false
+	}
+
+	if isMorse {
+		return morse.ToText(input), nil
+	}
+
+	upperInput := strings.ToUpper(input)
+
+	for _, r := range upperInput {
+		if _, ok := morse.DefaultMorse[r]; !ok {
+			return "", fmt.Errorf("Символ %c не поддерживается", r)
+		}
+	}
+
+	return morse.ToMorse(upperInput), nil
+}
